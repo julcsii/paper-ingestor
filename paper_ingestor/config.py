0afc85b6a7a3a78a3f5489be11df14955d7fb52f
Julia Hermann <julia.hermann@runtastic.com>
2022-07-22 20:14:41 +0200

    Save output as jsonline in s3

--- paper_ingestor/config.py
+++ paper_ingestor/config.py
@@ -7,5 +7,10 @@ config = {
             "samples_s3_key": "s2ag/samples/papers/papers-sample.jsonl.gz",
             "samples_local_path": "data/s2ag/samples/papers/papers-sample.jsonl.gz"
         }
+    },
+    "output": {
+        "bucket": "enriched-papers",
+        "local_path": "data/s2ag/output/papers-sample.jsonl.gz",
+        "s3_key": "s2ag/output/papers-sample.jsonl.gz"
     }
 }
--- paper_ingestor/main.py
+++ paper_ingestor/main.py
@@ -17,6 +17,20 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 
+def main():
+    logger.info("Starting with configurations: %s", config)
+    logger.info("Loading papers.")
+    sample_s2ag_papers = load_papers()
+    sample_s2ag_papers_deduped_df = deduplicate_papers(sample_s2ag_papers)
+    save_df_as_jsonl(sample_s2ag_papers_deduped_df, config["output"]["local_path"])
+    upload_output_to_s3(
+        config["output"]["bucket"],
+        config["output"]["local_path"],
+        config["output"]["s3_key"]
+    )
+    logger.info("Completed.")
+
+
 def load_papers(source="s2ag", sample=True) -> List[Dict]:
     """Load papers from source."""
     if source != "s2ag":
@@ -41,14 +55,30 @@ def load_papers(source="s2ag", sample=True) -> List[Dict]:
     return papers
 
 
+def deduplicate_papers(papers: List[Dict]) -> pd.DataFrame:
+    """Deduplicate list of papers by the id and return as DF."""
+    logger.info("Deduplicating papers by the id.")
+    return pd.DataFrame(papers).drop_duplicates(subset=["uniqueId"])
+
+
 def download_data_file_from_s3(bucket: str, s3_file_path: str, local_file_path: str):
     """Download file from S3 and create required local folders if they don't exist."""
     logger.info("Downloading %s from %s bucket in S3 to %s.", bucket, s3_file_path, local_file_path)
     s3 = boto3.resource('s3')
-    local_folders = "/".join(local_file_path.split("/")[:-1])
+    make_non_existing_dirs(local_file_path)
+    s3.Bucket(bucket).download_file(s3_file_path, local_file_path)
+
+
+def make_non_existing_dirs(local_file_path: str):
+    """Given a UNIX file path create non existing directories."""
+    local_folders = extract_folder_from_file_path(local_file_path)
     logger.info("Creating directories %s.", local_folders)
     os.makedirs(local_folders, exist_ok=True)
-    s3.Bucket(bucket).download_file(s3_file_path, local_file_path)
+
+
+def extract_folder_from_file_path(file_path: str) -> str:
+    """Given a local UNIX file path extract the directory path."""
+    return "/".join(file_path.split("/")[:-1])
 
 
 def create_paper_from_s2ag(paper_raw: Dict) -> Paper:
@@ -65,10 +95,20 @@ def create_paper_from_s2ag(paper_raw: Dict) -> Paper:
         inCitations=[]
     )
 
+
+def save_df_as_jsonl(df: pd.DataFrame, local_file_path: str):
+    """Save DataFrame as compressed JSONL."""
+    logger.info("Saving locally as json lines to %s.", local_file_path)
+    make_non_existing_dirs(local_file_path)
+    df.to_json(local_file_path, orient='records', lines=True, compression="gzip")
+
+
+def upload_output_to_s3(bucket: str, local_file_path: str, s3_file_path: str):
+    """Upload a local file to an S3 bucket."""
+    logger.info("Uploading %s to %s bucket in S3 with %s key.", local_file_path, bucket, s3_file_path)
+    s3 = boto3.client('s3')
+    s3.upload_file(local_file_path, bucket, s3_file_path)
+
+
 if __name__=="__main__":
-    logger.info("Starting with configurations: %s", config)
-    logger.info("Loading papers.")
-    sample_s2ag_papers = load_papers()
-    logger.info("Deduplicating papers by the id.")
-    sample_s2ag_papers_df = pd.DataFrame(sample_s2ag_papers).drop_duplicates(subset=["uniqueId"])
-    logger.info(sample_s2ag_papers_df.head())
+    main()
