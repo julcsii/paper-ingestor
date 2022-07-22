aa7d1b37ef44431e67b164db1693b1be080a8144
Julia Hermann <julia.hermann@runtastic.com>
2022-07-22 19:21:43 +0200

    Add simple deduplication with pandas

--- paper_ingestor/main.py
+++ paper_ingestor/main.py
@@ -5,12 +5,15 @@ import os
 from typing import Dict, List
 
 import boto3
+import pandas as pd
 
 from paper_ingestor.config import config
 from paper_ingestor.models.paper import Paper
 
 logging.basicConfig(
-    format=f"[{config['app_name']}] %(asctime)s - %(name)s - %(levelname)s - %(message)s", level=config["log_level"])
+    format=f"[{config['app_name']}] %(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=config["log_level"]
+)
 logger = logging.getLogger()
 
 
@@ -20,9 +23,9 @@ def load_papers(source="s2ag", sample=True) -> List[Dict]:
         raise NotImplementedError("Sorry, only 's2ag' source is supported.")
     if not sample:
         raise NotImplementedError("Sorry, only sample data loading is supported.")
-    papers = []
     if source == "s2ag":
         if sample:
+            papers = []
             samples_local_path = config["source"][source]["samples_local_path"]
             if not os.path.exists(samples_local_path):
                 download_data_file_from_s3(
@@ -39,6 +42,7 @@ def load_papers(source="s2ag", sample=True) -> List[Dict]:
 
 
 def download_data_file_from_s3(bucket: str, s3_file_path: str, local_file_path: str):
+    """Download file from S3 and create required local folders if they don't exist."""
     logger.info("Downloading %s from %s bucket in S3 to %s.", bucket, s3_file_path, local_file_path)
     s3 = boto3.resource('s3')
     local_folders = "/".join(local_file_path.split("/")[:-1])
@@ -64,4 +68,7 @@ def create_paper_from_s2ag(paper_raw: Dict) -> Paper:
 if __name__=="__main__":
     logger.info("Starting with configurations: %s", config)
     logger.info("Loading papers.")
-    papers = load_papers()
+    sample_s2ag_papers = load_papers()
+    logger.info("Deduplicating papers by the id.")
+    sample_s2ag_papers_df = pd.DataFrame(sample_s2ag_papers).drop_duplicates(subset=["uniqueId"])
+    logger.info(sample_s2ag_papers_df.head())
